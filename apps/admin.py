e3978ef62587de52236a907beb40031594ead20c
sujitpal <sujit.pal@comcast.net>
2021-03-29 19:44:09 -0700

    fix: registered new models to admin

--- apps/admin.py
+++ apps/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from .models import Attendee, Paper, Review
+from .models import (
+    Organization,
+    TimeZone,
+    Attendee, 
+    PaperType,
+    PaperTheme,
+    Paper, 
+    ReviewScore,
+    RejectionReason,
+    Review,
+)
 
 # Register your models here.
+admin.site.register(Organization)
+admin.site.register(TimeZone)
 admin.site.register(Attendee)
+admin.site.register(PaperType)
+admin.site.register(PaperTheme)
 admin.site.register(Paper)
+admin.site.register(ReviewScore)
+admin.site.register(RejectionReason)
 admin.site.register(Review)
